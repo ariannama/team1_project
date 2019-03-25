fd2e792b16cf2bd6f8b4afd304873157bdad76a0
Arianna <ariannamarrocu55@gmail.com>
2019-03-25 17:03:19 +0000

    Finished home page

--- js/index.js
+++ js/index.js
@@ -39,8 +39,4 @@ $( document ).ready(function(){
 
   $('body').removeClass('fade-out');
 
-  $('#lusu').on('click', function () {
-    window.open = "https://lancastersu.co.uk/groups/lu-mental-health";
-  })
-
 })
